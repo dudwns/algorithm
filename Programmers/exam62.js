c1fe913d28229d6071d6d9bf70bc6b2b87145809
yeongjun Kim <kyjun519@gmail.com>
2023-04-27 23:22:55 +0900

    A로 B 만들기

---
+++ Programmers/exam62.js
@@ -0,0 +1,16 @@
+function solution(before, after) {
+  let count = 0;
+  const beforeArray = [...before];
+  const afterArray = [...after];
+  for (let i = 0; i < before.length; i++) {
+    for (let j = 0; j < after.length; j++) {
+      if (beforeArray[i] === afterArray[j]) {
+        afterArray.splice(j, 1);
+        count++;
+        break;
+      }
+    }
+  }
+
+  return count === after.length ? 1 : 0;
+}
