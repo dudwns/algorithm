0a3c01379b3a868eb4a941ed2a44bad28dc84f27
yeongjun Kim <kyjun519@gmail.com>
2023-05-08 13:41:25 +0900

    숨어있는 숫자의 덧셈 (2)

--- Programmers/exam68.js
+++ Programmers/exam68.js
@@ -1,6 +1,6 @@
 function solution(my_string) {
   let answer = 0;
-  const regex = /[a-zA-z]/gi;
+  const regex = /[a-z]/gi;
   let str = my_string.replaceAll(regex, " ").split(" ");
   str.forEach((v) => (Number(v) ? (answer += Number(v)) : null));
   return answer;
