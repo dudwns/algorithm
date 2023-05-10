b40fd168b2124a9664c7a77fbbe0dbaeb9243968
yeongjun Kim <kyjun519@gmail.com>
2023-05-10 23:26:14 +0900

    컨트롤 제트

---
+++ Programmers/exam71.js
@@ -0,0 +1,8 @@
+function solution(s) {
+  let answer = 0;
+  array = s.split(" ");
+  array.forEach((v, index) => {
+    v !== "Z" ? (answer += Number(v)) : (answer -= array[index - 1]);
+  });
+  return answer;
+}
