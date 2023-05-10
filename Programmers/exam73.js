86c89162a89bddb50328081ca550c9fb3ff766d5
yeongjun Kim <kyjun519@gmail.com>
2023-05-10 23:27:09 +0900

    공 던지기

---
+++ Programmers/exam73.js
@@ -0,0 +1,9 @@
+function solution(numbers, k) {
+  var answer = 1;
+  for (let i = 1; i < k; i++) {
+    answer += 2;
+    if (answer === numbers.length + 1) answer = 1;
+    else if (answer === numbers.length + 2) answer = 2;
+  }
+  return answer;
+}
