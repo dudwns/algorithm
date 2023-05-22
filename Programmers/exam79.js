775ba9c9df1ea3bca9f6c689bc0b23edfaf79f73
yeongjun Kim <kyjun519@gmail.com>
2023-05-22 15:26:44 +0900

    삼각형의 완성조건 (2)

---
+++ Programmers/exam79.js
@@ -0,0 +1,14 @@
+function solution(sides) {
+  let answer = 0;
+  let max = Math.max(...sides);
+  let min = Math.min(...sides);
+  for (let i = 1; i <= max; i++) {
+    if (i + min > max) {
+      answer++;
+    }
+  }
+  for (let i = max + 1; i < max + min; i++) {
+    answer++;
+  }
+  return answer;
+}
