a10e7a58ec2e63ebefbd55442ace3e9f498ec0eb
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-07-18 12:52:54 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,4 @@
 function solution(s) {
-    var answer = "";
     const num = s.split(" ").map((n) => +n);
-    answer += Math.min(...num);
-    answer += " "+Math.max(...num);
-    return answer;
+    return `${Math.min(...num)} ${Math.max(...num)}`;
 }
