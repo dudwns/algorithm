4a667fefdfc2cbf9f3a15f74a98780da9278bede
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-07-24 08:00:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,23 +1,22 @@
 function solution(id_list, report, k) {
-    var answer = [];
     const users = new Map();
     const mails = new Map();
     // 중복 제거 후 각각 배열로 만듦
     const reports = [...new Set(report)].map(report => report.split(" "));
 
     // Map 객체에 id별로 0으로 초기화
-    for( let id of id_list){
+    for(const id of id_list){
         users.set(id, 0);
         mails.set(id, 0);
     }
  
     // reports를 순회하면서 신고 횟수를 증가
-    for (let report of reports){
+    for (const report of reports){
         users.set(report[1], users.get(report[1]) + 1);
     }
     
     // reports를 순회하면서 k 이상 신고한 사람을 신고했으면 mails의 count를 증가
-    for (let report of reports){
+    for (const report of reports){
         if(users.get(report[1]) >= k) {
             mails.set(report[0], mails.get(report[0]) + 1);
         }
