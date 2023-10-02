de8fb858c2710d04b4acc3be7adf4ff92e21e06e
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-10-03 03:45:17 +0900

    [Bronze V] Title: A×B, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const data = input[0].split(' ');
+
+const a = Number(data[0]);
+const b = Number(data[1]);
+
+console.log(a * b);
