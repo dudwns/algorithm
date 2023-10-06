c301370cc811a5378a0bd20f7658ced5607cfe59
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-10-06 16:58:44 +0900

    [Silver V] Title: K번째 수, Time: 4620 ms, Memory: 566756 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+arr.sort((a, b) => a - b);
+console.log(arr[k - 1]);
