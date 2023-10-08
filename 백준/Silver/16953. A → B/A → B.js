11b0698e5d2dbc85716b8e1769a90eba355c2365
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-10-08 13:12:48 +0900

    [Silver II] Title: A → B, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let [a, b] = input[0].split(' ').map(Number);
+let result = 1;
+let flag = false;
+
+while(a <= b) {
+    if(a === b) {
+        flag = true;
+        break;
+    }
+    if(b % 2 === 0) {
+        b = parseInt(b / 2);
+    } else if (b % 10 === 1) {
+        b = parseInt(b / 10);
+    }
+    else break;
+    result++;
+}
+
+if(flag) console.log(result);
+else console.log(-1);
