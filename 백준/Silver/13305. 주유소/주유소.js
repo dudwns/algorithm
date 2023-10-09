edaed2eab2988546ba19828c0b458112159ee27b
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-10-09 23:13:06 +0900

    [Silver III] Title: 주유소, Time: 212 ms, Memory: 20420 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -4,15 +4,16 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const n = Number(input[0]);
 let dist = input[1].split(' ').map(Number);
 let cost = input[2].split(' ').map(Number);
-
-for(let i = 0; i < n; i++) {
-    let min = cost[0];
-    min = Math.min(min, cost[i]);
-    cost[i] = min;
+let min = cost[0];
+for(let i = 1; i < n; i++){
+    if(cost[i] > min) {
+        cost[i] = min;
+    } else {
+        min = cost[i];
+    }
 }
-
 let answer = 0;
-for(let i = 0; i < n - 1; i++) {
-    answer += cost[i] * dist[i];
+for(let i = 0; i < n - 1; i++){
+    answer += dist[i] * cost[i];
 }
 console.log(answer);
