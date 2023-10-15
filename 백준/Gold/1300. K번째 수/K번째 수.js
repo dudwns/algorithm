902469aeacec3116a94ec8fe1a434204e3ae19ae
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-10-15 23:32:13 +0900

    [Gold I] Title: K번째 수, Time: 812 ms, Memory: 62740 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const n = Number(input[0]);
+const k = Number(input[1]);
+
+let start = 1;
+let end = 10 ** 10;
+
+let result = 0;
+while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    let total = 0;
+    for(let i = 1; i <= n; i++) {
+        total += Math.min(parseInt(mid / i), n)
+    }
+    if (total >= k) {
+        result = mid;
+        end = mid - 1;
+    } else {
+        start = mid + 1;
+    }
+}
+console.log(result);
