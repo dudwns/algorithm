37ab0cb2c864152e3c0f47a3d6f3ec6f4f0c4d29
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2024-02-09 16:45:09 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    let answer = [];
+    let str = s.split(' ');
+    for (word of str) {
+        let keyword = '';
+        for(let i = 0; i < word.length; i++) {
+            
+            if(i == 0) {
+                keyword += word[i].toUpperCase(); 
+            }
+            else {
+                keyword += word[i].toLowerCase(); 
+            }
+        } answer.push(keyword);
+    }
+    return answer.join(' ');
+}
