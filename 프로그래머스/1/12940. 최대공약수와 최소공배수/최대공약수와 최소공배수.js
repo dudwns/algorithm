903f0ecab2edd688fad24a49ca39ef92731fcb16
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2024-02-14 21:13:31 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,12 @@
+function solution(n, m) {
+    const gcf = (a, b) => {
+        if(b === 0) {
+            return a
+        }
+        return gcf(b, a % b)
+    }
+    
+    const lcm = (a, b) => (a * b) / gcf(a, b);
+    
+    return [gcf(n, m), lcm(n, m)];
+}
