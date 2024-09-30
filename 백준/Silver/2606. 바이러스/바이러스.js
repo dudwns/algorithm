80564854be34ed7483d59b4aca46976d57007424
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2024-09-30 23:39:31 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 9444 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -2,17 +2,16 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 let n = Number(input[0]);
 let m = Number(input[1]);
+let cnt = 0;
 let graph = [];
-
 for(let i = 1; i <= n; i++) graph[i] = [];
 for(let i = 2; i <= m + 1; i++) {
     let [x, y] = input[i].split(' ').map(Number);
     graph[x].push(y);
     graph[y].push(x);
-} 
+}
 
 let visited = new Array(n + 1).fill(false);
-let cnt = 0;
 function dfs(x) {
     visited[x] = true;
     cnt++;
